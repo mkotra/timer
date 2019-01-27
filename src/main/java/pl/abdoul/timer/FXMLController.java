b64f0f6fcad04bd2dc7d47e38285e1c8a897c0ae
Michal <kotra.michal@gmail.com>
2019-01-27 18:20:40 +0100

    refactor

--- src/main/java/pl/abdoul/timer/FXMLController.java
+++ src/main/java/pl/abdoul/timer/FXMLController.java
@@ -1,10 +1,8 @@
 package pl.abdoul.timer;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Timer;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -13,10 +11,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
+import static pl.abdoul.timer.Constants.*;
 
 public class FXMLController {
 
@@ -24,60 +25,83 @@ public class FXMLController {
             .toLowerCase()
             .startsWith("windows");
 
+    private static final ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(1);
+
+    private ScheduledFuture<?> task;
+
     @FXML
-    private Button button;
+    private Slider slider;
 
     @FXML
     private Label label;
 
     @FXML
-    private Slider slider;
-    
+    private Button runButton;
+
+    @FXML
+    private Button cancelButton;
+
     public void initialize() {
+        setMode(false);
         slider.valueProperty().addListener(e -> {
-              label.setText(formatValue() + "");
+            label.setText(formatValue() + "");
         });
     }
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) throws InterruptedException, IOException {
-        button.setDisable(true);
+    private void run(ActionEvent event) throws InterruptedException, IOException {
         double target = slider.getValue() * 60;
-        final AtomicInteger i = new AtomicInteger();
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        ScheduledFuture<?> scheduleAtFixedRate = executor.scheduleAtFixedRate(() -> {
-            var value = i.incrementAndGet();
-            Platform.runLater(() -> {
-                label.setText(value + " / " + formatValue());
-            });
-            if (value > target) {
-                shutdown();
-            }
-        }, 0, 1, TimeUnit.SECONDS);
-
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Poweroff in " + formatValue() + " minutes.");
+        alert.setContentText("Are you ok with this?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            setMode(true);
+            final AtomicInteger i = new AtomicInteger();
+            task = EXECUTOR.scheduleAtFixedRate(() -> {
+                var value = i.incrementAndGet();
+                Platform.runLater(() -> {
+                    label.setText(value/60 + " / " + formatValue());
+                });
+                if (value > target) {
+                    shutdown();
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+        }
     }
 
-    public int formatValue() {
-        return (int) Math.round(slider.getValue());
+    @FXML
+    private void cancel(ActionEvent event) throws InterruptedException, IOException {
+        task.cancel(true);
+        setMode(false);
     }
 
-    public void shutdown() {
+    private void shutdown() {
         ProcessBuilder builder = new ProcessBuilder();
         if (IS_WINDOWS) {
-            builder.command("cmd.exe", "/c", "shutdown");
+            builder.command(SHUTDOWN_WINDOWS);
         } else {
-            builder.command("sh", "-c", "poweroff");
+            builder.command(SHUTDOWN_LINUX);
         }
-        builder.directory(new File(System.getProperty("user.home")));
+        builder.directory(new File(System.getProperty(USER_HOME)));
         Process process;
         try {
             process = builder.start();
             int exitCode = process.waitFor();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } catch (InterruptedException ex) {
+        } catch (IOException | InterruptedException ex) {
+            setMode(false);
             ex.printStackTrace();
         }
+    }
 
+    private void setMode(boolean running) {
+        label.setText("");
+        runButton.setDisable(running);
+        cancelButton.setDisable(!running);
+    }
+
+    private int formatValue() {
+        return (int) Math.round(slider.getValue());
     }
 }
